9cdfa82d51270c7e721180c8f753e7f9c14643d6
Naem Haddad <naemhd@gmail.com>
2021-07-02 22:00:28 -0500

    adding lab10

--- lab10/public/angular-app/app.js
+++ lab10/public/angular-app/app.js
@@ -6,11 +6,17 @@ function config($routeProvider){
         controller:"jobsListing",
         controllerAs:"vm"
     })
+    .when("/jobs/create",{
+        templateUrl:"angular-app/create-job/create.html",
+        controller:"CreateJob",
+        controllerAs:"vm"
+    })
     .when("/jobs/:jobId",{
         templateUrl:"angular-app/jobs-detail/detail.html",
         controller:"jobsDetail",
         controllerAs:"vm"
     })
+    
     .otherwise({
         redirectTo:"/"
     })
--- lab10/public/angular-app/jobs-data-factory/jobs.data.factory.js
+++ lab10/public/angular-app/jobs-data-factory/jobs.data.factory.js
@@ -3,13 +3,17 @@ angular.module("meanJobs").factory("JobsFactory",JobsFactory)
 function JobsFactory($http){
     return {
         getAll:getAll,
-        getOne:getOne
+        getOne:getOne,
+        postJob:postJob
     }
     function getAll(){
         return $http.get("/api/jobs").then(completed).catch(failed);
     }
     function getOne(jobId){
         return $http.get("/api/jobs/"+jobId).then(completed).catch(failed);
+    }
+    function postJob(postData){
+        return $http.post("/api/jobs",postData).then(completed).catch(failed);
     }
     function completed(response){
         console.log(response);
